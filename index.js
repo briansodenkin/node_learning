c6b96b088dda10d7bff5925306f8f2eb6f50dbe6
briansodenkin <briansodenkin@gmail.com>
2022-07-28 22:05:50 +0800

    Add nodejs files

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log(__dirname)
+// setInterval(() => {
+//     console.log('hello')
+// },1000)
+
+// Modules
+
+const names = require('./names')
+console.log(names)
+
+// sayHi('GG')
---
+++ names.js
@@ -0,0 +1,8 @@
+const test = 'test'
+
+// console.log(module)
+
+
+module.exports = {
+ test
+}
